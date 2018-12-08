fef78e70bc27e0934fcac3d9976da40d3da3d6d0
Andrew Jones <joneseyit@protonmail.com>
2018-12-08 13:00:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (blocks) => {
+  let result = Math.abs(42 - blocks)
+
+  return result
+}
+
+const distanceFromHqInFeet = (blocks) => {
+  result = distanceFromHqInBlocks(blocks) * 264
+  return result
+}
+
+const distanceTravelledInFeet = (b1, b2) => {
+  blocks = Math.abs(b1 - b2)
+  result = distanceFromHqInFeet(blocks)
+  return result
+}
